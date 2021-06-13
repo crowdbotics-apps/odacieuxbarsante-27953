91d8983c6c9f277d9afde11f660e7388b1214fc2
Crowdbotics <team@crowdbotics.com>
2021-06-13 18:37:40 +0000

    Update models

--- odacieuxbarsante_27953/wsgi.py
+++ odacieuxbarsante_27953/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odacieuxbarsante_27953.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "odacieuxbarsante_27953.settings")
 
 application = get_wsgi_application()
